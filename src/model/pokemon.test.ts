b8a2a3beb6744b6a162722f781e9ec11602233b7
Raymond Shiner <raymondshiner@gmail.com>
2022-10-15 13:48:04 -0700

    add pokemon type

--- src/model/pokemon.test.ts
+++ src/model/pokemon.test.ts
@@ -1,11 +1,20 @@
 import { Pokemon } from "./pokemon"
+import { PokemonType } from "./pokemonTypes"
 
 describe("Pokemon Object", () => {
   it.each(["Bulbasaur", "Charmander", "Squirtle"])(
-    "should always have a name",
+    "should always have the name defined",
     (pokemonName: string) => {
-      const pokemon = new Pokemon(pokemonName)
+      const pokemon = new Pokemon(pokemonName, PokemonType.Normal)
       expect(pokemon.name).toEqual(pokemonName)
+    }
+  )
+
+  it.each([PokemonType.Grass, PokemonType.Fire, PokemonType.Water])(
+    "should always have the primary type defined",
+    (expectedPrimaryType: PokemonType) => {
+      const pokemon = new Pokemon("Ditto", expectedPrimaryType)
+      expect(pokemon.primaryType).toEqual(expectedPrimaryType)
     }
   )
 })
--- src/model/pokemon.ts
+++ src/model/pokemon.ts
@@ -1,10 +1,19 @@
+import { PokemonType } from "./pokemonTypes"
+
 export class Pokemon {
   private _name: string
-  constructor(pokemonName: string) {
+  private _primaryType: PokemonType
+
+  constructor(pokemonName: string, pokemonType: PokemonType) {
     this._name = pokemonName
+    this._primaryType = pokemonType
   }
 
   public get name() {
     return this._name
+  }
+
+  public get primaryType() {
+    return this._primaryType
   }
 }
