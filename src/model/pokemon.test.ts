9f97658d684c2a40860cc79372035f5e46c36e6a
Raymond Shiner <raymondshiner@gmail.com>
2022-10-15 14:01:08 -0700

    add secondary types and number

--- src/model/pokemon.test.ts
+++ src/model/pokemon.test.ts
@@ -5,7 +5,7 @@ describe("Pokemon Object", () => {
   it.each(["Bulbasaur", "Charmander", "Squirtle"])(
     "should always have the name defined",
     (pokemonName: string) => {
-      const pokemon = new Pokemon(pokemonName, PokemonType.Normal)
+      const pokemon = new Pokemon(pokemonName, 0, PokemonType.Normal)
       expect(pokemon.name).toEqual(pokemonName)
     }
   )
@@ -13,8 +13,35 @@ describe("Pokemon Object", () => {
   it.each([PokemonType.Grass, PokemonType.Fire, PokemonType.Water])(
     "should always have the primary type defined",
     (expectedPrimaryType: PokemonType) => {
-      const pokemon = new Pokemon("Ditto", expectedPrimaryType)
+      const pokemon = new Pokemon("Ditto", 0, expectedPrimaryType)
       expect(pokemon.primaryType).toEqual(expectedPrimaryType)
+    }
+  )
+
+  it.each([1, 2, 3])(
+    "should always have the number defined",
+    (expectedNumber: number) => {
+      const pokemon = new Pokemon("Ditto", expectedNumber, PokemonType.Normal)
+      expect(pokemon.number).toEqual(expectedNumber)
+    }
+  )
+
+  it("should have no secondary Type by default", () => {
+    const pokemon = new Pokemon("Bulbasaur", 0, PokemonType.Grass)
+    expect(pokemon.secondaryType).toBe(undefined)
+  })
+
+  it.each([PokemonType.Grass, PokemonType.Fire, PokemonType.Water])(
+    "should always have the secondary Type defined",
+    (expectedSecondaryType: PokemonType) => {
+      const pokemon = new Pokemon(
+        "Bulbasaur",
+        0,
+        PokemonType.Normal,
+        expectedSecondaryType
+      )
+
+      expect(pokemon.secondaryType).toBe(expectedSecondaryType)
     }
   )
 })
--- src/model/pokemon.ts
+++ src/model/pokemon.ts
@@ -2,18 +2,35 @@ import { PokemonType } from "./pokemonTypes"
 
 export class Pokemon {
   private _name: string
+  private _number: number
   private _primaryType: PokemonType
+  private _secondaryType: PokemonType | undefined
 
-  constructor(pokemonName: string, pokemonType: PokemonType) {
-    this._name = pokemonName
-    this._primaryType = pokemonType
+  constructor(
+    name: string,
+    number: number,
+    primaryType: PokemonType,
+    secondaryType?: PokemonType
+  ) {
+    this._name = name
+    this._number = number
+    this._primaryType = primaryType
+    this._secondaryType = secondaryType
   }
 
   public get name() {
     return this._name
   }
 
+  public get number() {
+    return this._number
+  }
+
   public get primaryType() {
     return this._primaryType
+  }
+
+  public get secondaryType() {
+    return this._secondaryType
   }
 }
