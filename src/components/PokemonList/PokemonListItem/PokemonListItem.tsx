773c2e0b35b954d9c71ff290831398b677391b14
Raymond Shiner <raymondshiner@gmail.com>
2022-10-15 17:17:19 -0700

    add basic list item styling

--- src/components/PokemonList/PokemonListItem/PokemonListItem.tsx
+++ src/components/PokemonList/PokemonListItem/PokemonListItem.tsx
@@ -1,20 +1,73 @@
 import React from "react"
 import { Pokemon } from "../../../model/pokemon/pokemon"
 
+import styled from "styled-components"
+
 export const PokemonListItem = (props: { pokemon: Pokemon }) => {
   const { pokemon } = props
   const formattedNumber: string = formatPokemonNumber(pokemon.number)
 
   return (
-    <>
-      <div>{pokemon.name}</div>
-      <div>{formattedNumber}</div>
-      <div>{pokemon.primaryType}</div>
-      <div>{pokemon.secondaryType}</div>
-      <img alt={pokemon.name} src={pokemon.imageUrl} />
-    </>
+    <ListItemWrapper>
+      <Image alt={pokemon.name} src={pokemon.imageUrl} />
+      <Stack>
+        <SpaceBetween>
+          <Name>{pokemon.name}</Name>
+          <Number>{formattedNumber}</Number>
+        </SpaceBetween>
+        <Flexbox>
+          <div>{pokemon.primaryType}</div>
+          {pokemon.secondaryType && <div>/</div>}
+          <div>{pokemon.secondaryType}</div>
+        </Flexbox>
+      </Stack>
+    </ListItemWrapper>
   )
 }
+
+const Flexbox = styled.div`
+  display: flex;
+  gap: 0.5rem;
+`
+
+const Stack = styled(Flexbox)`
+  width: 100%;
+  flex-direction: column;
+`
+
+const SpaceBetween = styled(Flexbox)`
+  justify-content: space-between;
+`
+
+const ListItemWrapper = styled(Flexbox)`
+  display: flex;
+  gap: 1rem;
+  border-radius: 1rem;
+  background-color: white;
+  border: 1px solid black;
+  width: 30rem;
+  padding: 1rem;
+`
+
+const Image = styled.img`
+  border-radius: 1rem;
+  background-color: lightgray;
+  box-shadow: 1px 1px 1px 1px darkgray;
+  height: 10rem;
+  width: 10rem;
+`
+
+const Name = styled.div`
+  font-size: 2rem;
+  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+  width: 100%;
+`
+const Number = styled.div`
+  font-size: 1.5rem;
+  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
+  font-style: italic;
+  color: grey;
+`
 
 const formatPokemonNumber = (number: number) => {
   const numberIsOneDigit = number < 10
